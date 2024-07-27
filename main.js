d646a7dac6a129b31f40b682ba8b1f7d72219416
CaroSaFe <carolina.rsf35@gmail.com>
2024-07-26 23:25:50 -0400

    geolocation,spinner, container, css

--- main.js
+++ main.js
@@ -1,29 +1,51 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let gridX = 37.7719;
-    let gridY = -87.1112;
-    const weatherAPI = (Xpoint, Ypoint) => {
-        fetch(`https://api.weather.gov/points/${gridX},${gridY}`)
-        .then((response) => response.json())
-        .then((data) => {
-            // console.log(data)
-            
-            fetch(`${data.properties.forecastHourly}`)
-                .then((response2) => response2.json())
-                .then((data2) => {
-                    console.log(data2);
-                    const currentFC = document.createElement("p")
-                    currentFC.textContent = data2.properties.periods[0].shortForecast + '  ' + Date()
-                    document.querySelector('body').appendChild(currentFC)
-                })
-        })
-        // console.log(link);
-    }
-    // const hourlyLink = ;
-    // fetch(`${weatherAPI(gridX,gridY)}`)
-    //         .then((response) => response.json())
-    //         .then((data) => {
-        //             console.log(data);
-        //         })
-    console.log(weatherAPI(gridX,gridY));
 
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector('body');
+    const spinner = document.getElementById('spinner');
+    
+    container.style.visibility = "hidden";
+    spinner.style.visibility = "visible";
+
+    setTimeout(() => {
+        spinner.style.visibility = "hidden";
+        container.style.visibility = "visible";
+    }, 3000);
+
+
+    const dateFor = new Date();
+    const formattedDate = dateFor.toLocaleString('en-US', { timeZoneName: "short" });
+    
+    const weatherAPI = (Xpoint, Ypoint) => {
+            fetch(`https://api.weather.gov/points/${Xpoint},${Ypoint}`)
+            .then((response) => response.json())
+            .then((data) => {
+                // console.log(data)
+                
+                fetch(`${data.properties.forecastHourly}`)
+                    .then((response2) => response2.json())
+                    .then((data2) => {
+                        console.log(data2);
+                        const currentFC = document.createElement('div')
+                        currentFC.textContent = `${data2.properties.periods[0].temperature}°F ${data2.properties.periods[0].shortForecast}`
+                        currentFC.id = "currentForecast"
+                        document.querySelector('.container').appendChild(currentFC)
+
+                        const city = document.createElement('div')
+                        city.textContent = `City: ${data.properties.relativeLocation.properties.city}`
+                        city.id ="city"
+                        document.querySelector('.container').appendChild(city)
+
+                        const date = document.createElement('div')
+                        date.textContent = `${formattedDate}`
+                        document.querySelector('.container').appendChild(date)
+                    })
+            })
+        }
+        
+         navigator.geolocation.getCurrentPosition(function(location) {
+            console.log("accuracy", location.coords.accuracy);
+            weatherAPI(location.coords.latitude,location.coords.longitude);
+          
+        })
+           
 });
